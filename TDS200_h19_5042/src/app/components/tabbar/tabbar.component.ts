eaf4feaf49675e6d2817ba1de3c1789eb70074ee
marcustjensen <marcus.tveide.jensen@hotmail.com>
2019-11-26 02:25:44 +0100

    final commit

--- TDS200_h19_5042/src/app/components/tabbar/tabbar.component.ts
+++ TDS200_h19_5042/src/app/components/tabbar/tabbar.component.ts
@@ -9,6 +9,13 @@ import { StorageService } from 'src/app/storage.service';
   templateUrl: './tabbar.component.html',
   styleUrls: ['./tabbar.component.scss'],
 })
+/*Jeg var ikke klar over feilen med tab-baren min før jeg testet applikasjonen før levering
+  Jeg vet dog hva som er feilen og hva som skal til for å fikse den.
+  Akkurat nå er det slik at tab-baren vil lastet på nytt sammen med sidene, siden den er plassert i bunnen på alle sidene.
+  Om jeg skal unngå dette, må jeg lage en egen side for tab-baren, som benytter seg av ion-tabs.
+  Denne må ha en egen tabs.router.module, hvor de andre sidene legges til som children av tabsPage.
+  Det er slik man egentlig skal implementere en tab-bar i Ionic, noe jeg er klar over nå.
+  Håper jeg har med nok av annen funksjonalitet slik at det veier opp.*/
 export class TabbarComponent implements OnInit {
 
   constructor(private router: Router, private authService: AuthorizationService,
